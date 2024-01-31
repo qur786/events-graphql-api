f634f61e64d930f960ceb7bc8b7ce6616e8c663e
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-01-31 12:42:42 +0530

    feat: setup express, tsc wactch, nodemon, concurrently

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+const PORT =
+  typeof process.env.PORT === "string"
+    ? Number.parseInt(process.env.PORT)
+    : 3000;
+
+app.use("/graphql", (_req, res) => res.send("Graphql"));
+
+app.listen(3000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
