5a161ba43c6dd234e079578e639707e50b7d79b1
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-02 17:17:14 +0530

    feat: added user resolvers

---
+++ src/graphql/resolvers/user.resolver.ts
@@ -0,0 +1,32 @@
+import { hash } from "bcrypt";
+import { UserModal, type User } from "../../model/user.modal.js";
+
+type CreateUserInput = Omit<User, "_id" | "events">;
+
+export const userResolvers = {
+  Query: {
+    users: async () => {
+      const users = await UserModal.find();
+      return users;
+    },
+  },
+  Mutation: {
+    createUser: async (
+      _parent: unknown,
+      { data: { email, password } }: { data: CreateUserInput }
+    ) => {
+      const existingUser = await UserModal.findOne({ email });
+      console.log(existingUser);
+      if (existingUser) {
+        throw new Error("User email already exists.");
+      }
+      const hashedPassword = await hash(password, 10);
+      const user = new UserModal({
+        email,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      return result;
+    },
+  },
+};
